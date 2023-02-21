dc524dde987dd6edc6d8cc0cfcde74f9bf198607
Hector Torres <h.s.torres96@gmail.com>
2023-02-20 20:47:57 -0800

    felt little destroyed by this one, done-ish though

--- solution3/main.js
+++ solution3/main.js
@@ -1,7 +1,7 @@
 const divResults = document.querySelector('#results');
 const wordResults = document.querySelector('#wordResults');
 const p = document.querySelector('p');
-const summary = document.querySelector('#summary');
+const summary = document.querySelector('section:last-of-type');
 
 const primaryColors = ['red', 'yellow', 'green', 'blue'];
 
@@ -12,19 +12,27 @@ fetch('data.json')
     for (const obj of data) {
       const div = document.createElement('div');
 
-      const img = document.createElement('img');
-      img.src = obj.icon;
-
       const h3 = document.createElement('h3');
+      h3.style.backgroundImage = `url(${obj.icon})`;
+      h3.classList.add('h3BackgroundImage');
+      h3.style.color = obj.color;
       h3.textContent = obj.category;
 
       const span = document.createElement('span');
       span.textContent = `${obj.score} / 100`;
 
-      div.append(img, h3, span);
+      div.append(h3, span);
       div.classList.add('summaryLogs', `${obj.color}`);
       summary.append(div);
     }
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Summary';
+
+    const button = document.createElement('button');
+    button.textContent = 'Continue';
+
+    summary.prepend(h2);
+    summary.append(button);
   })
   .catch((err) => {
     console.log(err);
